768ae3faf08da21ccd7c84e0059561ca5009a6d0
bmsnc <qjatjs98@gmail.com>
2023-07-16 00:07:50 +0900

    Add : 폴더 생성

---
+++ Backend/mate/src/main/java/mate/domain/User.java
@@ -0,0 +1,11 @@
+package mate.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id @GeneratedValue
+    private Long id;
+}
